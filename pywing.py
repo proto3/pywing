8ad440be6ec3e184a07810e068311a69a1eec60b
Lucas Felix <lucas@sogilis.com>
2017-06-26 15:07:33 +0200

    produce output

--- pywing.py
+++ pywing.py
@@ -166,7 +166,14 @@ class Airfoil:
         counter_a = 1
         counter_b = 1
         output = list()
-        output.append([self.points[0], airfoil.points[0]])
+        for i in xrange(4):
+            output.append(list())
+
+        output[0].append(self.points[0][0])
+        output[1].append(self.points[0][1])
+        output[2].append(airfoil.points[0][0])
+        output[3].append(airfoil.points[0][1])
+
         while(counter_a != len(self.points-1) and counter_b != len(airfoil.points-1)):
             npos_a = self.get_normalized_pos(counter_a)
             npos_b = airfoil.get_normalized_pos(counter_b)
@@ -184,32 +191,68 @@ class Airfoil:
 
             p_a = self.get_point(npos)
             p_b = airfoil.get_point(npos)
-            output.append([p_a, p_b])
+            # output.append([p_a, p_b])
+            output[0].append(p_a[0])
+            output[1].append(p_a[1])
+            output[2].append(p_b[0])
+            output[3].append(p_b[1])
 
         return np.array(output, float)
 ################################################################################
 
-root = Airfoil("ag455.dat")
+root = Airfoil("/home/lucas/fuselage.dat")
 root.normalize()
 
-tip = Airfoil("clarky.dat")
+tip = Airfoil("/home/lucas/fuselage.dat")
 tip.normalize()
 
-length = 180
-root *= length
-tip  *= 180+20
+length = 408
+root *= 408
+tip  *= 408
 
-root.dilate(2)
-root.points -= 20
+# tip.points = tip.points + [80.0, 0.0]
 
+root.dilate(1.0)
+tip.dilate(1.1)
+# root.points -= 20
+
+
+fusion = root.merge(tip)
+fusion[0] = length - fusion[0] + 2
+fusion[2] = length - fusion[2] + 2
+fusion[1] += 30
+fusion[3] += 30
+for i in xrange(len(fusion[0])):
+    plt.plot([fusion[0][i], fusion[2][i]], [fusion[1][i], fusion[3][i]])
+
+coeff_a_up = (fusion[1][1]-fusion[1][0])/(fusion[0][1]-fusion[0][0])
+plt.plot([0, fusion[0][0]], [fusion[1][0]-(fusion[0][0]*coeff_a_up),fusion[1][0]])
+
+coeff_b_up = (fusion[3][1]-fusion[3][0])/(fusion[2][1]-fusion[2][0])
+plt.plot([0, fusion[2][0]], [fusion[3][0]-(fusion[2][0]*coeff_b_up),fusion[3][0]])
+
+# print "G01 X0.000 Y%.3f U0.000 V%.3f F200" %(fusion[1][0]-(fusion[0][0]*coeff_a_up), fusion[3][0]-(fusion[2][0]*coeff_b_up))
+print "G01 X0.000 Y%.3f U0.000 V%.3f F200" %(fusion[3][0]-(fusion[2][0]*coeff_b_up), fusion[1][0]-(fusion[0][0]*coeff_a_up))
+
+for i in xrange(len(fusion[0])):
+    print "G01 X%.3f Y%.3f U%.3f V%.3f" %(fusion[2][i], fusion[3][i], fusion[0][i], fusion[1][i])
+    # print "G01 X%.3f Y%.3f U%.3f V%.3f" %(fusion[0][i], fusion[1][i], fusion[2][i], fusion[3][i])
+
+last = len(fusion[0]) - 1
+coeff_a_down = (fusion[1][last-1]-fusion[1][last])/(fusion[0][last-1]-fusion[0][last])
+plt.plot([0, fusion[0][last]], [fusion[1][last]-(fusion[0][last]*coeff_a_down),fusion[1][last]])
+
+coeff_b_down = (fusion[3][last-1]-fusion[3][last])/(fusion[2][last-1]-fusion[2][last])
+plt.plot([0, fusion[2][last]], [fusion[3][last]-(fusion[2][last]*coeff_a_down),fusion[3][last]])
+
+# print "G01 X0.000 Y%.3f U0.000 V%.3f" %(fusion[1][last]-(fusion[0][last]*coeff_a_down), fusion[3][last]-(fusion[2][last]*coeff_a_down))
+print "G01 X0.000 Y%.3f U0.000 V%.3f" %(fusion[3][last]-(fusion[2][last]*coeff_a_down), fusion[1][last]-(fusion[0][last]*coeff_a_down))
+
+root.points = [length+10, 0] + root.points*[-1,1]
+tip.points = [length+10, 0] + tip.points*[-1,1]
 root.plot()
 tip.plot()
 
-fusion = root.merge(tip)
-for e in fusion:
-    plt.plot([e[0][0],e[1][0]], [e[0][1],e[1][1]])
-
-
-plt.axis([-10, length+10, -length/2, length/4])
+plt.axis([0, length+20, -length/3, length/3])
 # plt.axis([-0.1, 1.1, -1, 1])
 plt.show()
