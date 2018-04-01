545d8f473dcee2b411ef3926c8f40da63401e9af
Lucas Felix <lucas@sogilis.com>
2018-04-01 18:45:41 +0200

    Create AirfoilManager to encapsulate airfoil related items

--- pywing.py
+++ pywing.py
@@ -7,34 +7,45 @@ import sys
 from airfoil import Airfoil
 
 airfoil_data_folder = QtCore.QDir.homePath() + "/.airfoils"
+airfoil_left = Airfoil()
+airfoil_right = Airfoil()
 
-class MyWidget(QtGui.QWidget):
+class AirfoilManager:
+    def __init__(self, airfoil, color, name):
+        self.airfoil = airfoil
+        self.curve_item = pg.PlotCurveItem([], [], pen=pg.mkPen(color=color, width=2))
+
+        self.load_btn = QtGui.QPushButton("Load " + name)
+        self.load_btn.clicked.connect(self.on_load)
+
+        self.rot_slider = QtGui.QSlider()
+        self.rot_slider.setMinimum(-90)
+        self.rot_slider.setMaximum(90)
+        self.rot_slider.setValue(0)
+        self.rot_slider.setTickPosition(QtGui.QSlider.TicksBelow)
+        self.rot_slider.setTickInterval(10)
+        self.rot_slider.valueChanged.connect(self.on_rot)
+
+    def on_load(self):
+        filename, _ = QtGui.QFileDialog.getOpenFileName(self.load_btn.parent(), "Open File", airfoil_data_folder, "All Files (*)")
+        if filename:
+            self.airfoil.load(filename)
+            self.curve_item.setData(self.airfoil.x, self.airfoil.y)
+
+    def on_rot(self):
+        self.airfoil.rotate(self.rot_slider.value())
+        self.curve_item.setData(self.airfoil.x, self.airfoil.y)
+
+class MainWidget(QtGui.QWidget):
     def __init__(self, filename = None):
         super().__init__()
 
-        left_color = (46, 134, 171)
-        right_color = (233, 79, 55)
-        self.airfoil_item_left = pg.PlotCurveItem([], [], pen=pg.mkPen(color=left_color, width=2))
-        self.airfoil_item_right = pg.PlotCurveItem([], [], pen=pg.mkPen(color=right_color, width=2))
-        self.airfoil_left = Airfoil()
-        self.airfoil_right = Airfoil()
-
-        load_left_btn = QtGui.QPushButton('Load left')
-        load_right_btn = QtGui.QPushButton('Load right')
-        load_left_btn.clicked.connect(self.on_load_left)
-        load_right_btn.clicked.connect(self.on_load_right)
-
-        self.left_rot_slider = QtGui.QSlider()
-        self.left_rot_slider.setMinimum(-90)
-        self.left_rot_slider.setMaximum(90)
-        self.left_rot_slider.setValue(0)
-        self.left_rot_slider.setTickPosition(QtGui.QSlider.TicksBelow)
-        self.left_rot_slider.setTickInterval(10)
-        self.left_rot_slider.valueChanged.connect(self.on_left_rot)
+        self.airfoil_left_view = AirfoilManager(airfoil_left,(46, 134, 171), "left")
+        self.airfoil_right_view = AirfoilManager(airfoil_right,(233, 79, 55), "right")
 
         plot = pg.PlotWidget()
-        plot.addItem(self.airfoil_item_left)
-        plot.addItem(self.airfoil_item_right)
+        plot.addItem(self.airfoil_left_view.curve_item)
+        plot.addItem(self.airfoil_right_view.curve_item)
 
         grid_alpha = 50
         grid_levels = [(10, 0), (5, 0), (1, 0)]
@@ -49,27 +60,12 @@ class MyWidget(QtGui.QWidget):
         plot.setAspectLocked()
 
         layout = QtGui.QGridLayout()
-        layout.addWidget(load_left_btn, 0, 0)
-        layout.addWidget(load_right_btn, 1, 0)
-        layout.addWidget(self.left_rot_slider, 2, 0)
+        layout.addWidget(self.airfoil_left_view.load_btn, 0, 0)
+        layout.addWidget(self.airfoil_left_view.rot_slider, 1, 0)
         layout.addWidget(plot, 0, 1, 3, 1)
+        layout.addWidget(self.airfoil_right_view.load_btn, 0, 2)
+        layout.addWidget(self.airfoil_right_view.rot_slider, 1, 2)
         self.setLayout(layout)
-
-    def open_airfoil(self, airfoil_item, airfoil):
-        filename, _ = QtGui.QFileDialog.getOpenFileName(self, "Open File", airfoil_data_folder, "All Files (*)")
-        if filename:
-            airfoil.load(filename)
-            airfoil_item.setData(airfoil.x, airfoil.y)
-
-    def on_load_left(self):
-        self.open_airfoil(self.airfoil_item_left, self.airfoil_left)
-
-    def on_load_right(self):
-        self.open_airfoil(self.airfoil_item_right, self.airfoil_right)
-
-    def on_left_rot(self):
-        self.airfoil_left.rotate(self.left_rot_slider.value())
-        self.airfoil_item_left.setData(self.airfoil_left.x, self.airfoil_left.y)
 
 if __name__ == '__main__':
     pg.setConfigOption('background', 'w')
@@ -77,6 +73,6 @@ if __name__ == '__main__':
     pg.setConfigOption('antialias', True)
 
     app = QtGui.QApplication([])
-    main_widget = MyWidget()
+    main_widget = MainWidget()
     main_widget.show()
     sys.exit(app.exec_())
