8707fae5c639f434f064df6772321f007323cb6c
Lucas Felix <lucas@sogilis.com>
2018-04-09 22:14:57 +0200

    compute and display fusion of airfoils

--- airfoil.py
+++ airfoil.py
@@ -13,8 +13,10 @@ class Airfoil:
         self.t = [0, 0]
         self.d = 0
         if filename is None :
-            self.file_data = self.mod_data = np.array([[],[]])
+            self.orig_data = self.trans_data = np.array([[],[]])
+            self.loaded = False
         else:
+            self.loaded = True
             self.load(filename)
     ###############################################
     def load(self, filename):
@@ -32,24 +34,28 @@ class Airfoil:
                     y.append(b)
             except ValueError:
                 pass
-        self.file_data = np.array([x, y])
+        self.orig_data = np.array([x, y])
 
         # normalize x between 0 and 1
-        min = self.file_data[0].min()
-        max = self.file_data[0].max()
-        self.file_data[0] -= min
-        if(max == min):
+        min_x = self.orig_data[0].min()
+        max_x = self.orig_data[0].max()
+        self.orig_data[0] -= min_x
+        if(max_x == min_x):
+            self.loaded = False
             return
-        self.file_data /= (max-min)
+        self.orig_data /= (max_x - min_x)
 
-        if(not self.compute_leading_edge()):
-            self.file_data = self.mod_data = np.array([[],[]])
+        if(not self.__compute_leading_edge()):
+            self.orig_data = self.trans_data = np.array([[],[]])
+            self.loaded = False
             return
 
+        self.orig_data[0] = -self.orig_data[0]
+        self.loaded = True
         self.__apply_transform()
     ###############################################
     def __str__(self):
-        return str(self.mod_data)
+        return str(self.trans_data)
     ###############################################
     def rotate(self, rotation):
         self.r = rotation
@@ -71,9 +77,9 @@ class Airfoil:
         self.d = radius
         self.__apply_transform()
     ###############################################
-    def __dilate(self):
-        x = self.mod_data[0]
-        y = self.mod_data[1]
+    def __dilate(self, radius):
+        x = self.orig_data[0]
+        y = self.orig_data[1]
 
         x = np.insert(x, 0, 2*x[0]-x[1])
         y = np.insert(y, 0, 2*y[0]-y[1])
@@ -84,126 +90,114 @@ class Airfoil:
         angle_a = np.arctan2(y[:-1]-y[1:], x[:-1]-x[1:])[:-1] # 1 to end-1
         angle_b = np.arctan2(y[1:]-y[:-1], x[1:]-x[:-1])[1:] # 1 to end-1
         diff = (angle_b - angle_a + 2*pi)%(2*pi)
-        self.mod_data[0] = x[1:-1] - self.d * np.cos(angle_a + diff/2)
-        self.mod_data[1] = y[1:-1] - self.d * np.sin(angle_a + diff/2)
+
+        self.dilate_data = np.stack((
+            x[1:-1] - radius * np.cos(angle_a + diff/2),
+            y[1:-1] - radius * np.sin(angle_a + diff/2)))
     ###############################################
-    def __apply_transform(self):
-        if(self.file_data.size == 0):
-            return
-        rrad = -self.r / 180 * pi
-        mat = np.array([[-self.s*cos(rrad), self.s*sin(rrad), self.t[0]+self.s],
+    def __refresh_transform_mat(self):
+        rrad = self.r / 180 * pi
+        self.tr_mat = np.array([[self.s*cos(rrad), -self.s*sin(rrad), self.t[0]+self.s],
                         [self.s*sin(rrad), self.s*cos(rrad), self.t[1]       ],
                         [0,                 0,                1               ]])
-        z_padded = np.pad(self.file_data, ((0, 1), (0, 0)), 'constant', constant_values=1)
-        self.mod_data = np.dot(mat, z_padded)[:-1]
-        self.__dilate()
     ###############################################
-    def compute_leading_edge(self):
-        edge_candidates = np.where(self.file_data[0] == 0)[0]
-        self.l_edge_idx = None
+    def __apply_transform(self):
+        if(not self.loaded):
+            return
+
+        self.__dilate(self.d / self.s)
+        self.__refresh_transform_mat()
+
+        z_padded = np.pad(self.dilate_data, ((0, 1), (0, 0)), 'constant', constant_values=1)
+        self.trans_data = np.dot(self.tr_mat, z_padded)[:-1]
+
+        self.get_points(self.get_point_chord_list())
+    ###############################################
+    def get_points(self, chord_point_list):
+        if(not self.loaded):
+            return
+
+        self.spe_data = np.array([[],[]])
+        for i in chord_point_list:
+            w = self.get_point(i)
+            self.spe_data = np.append(self.spe_data, [[w[0]], [w[1]]], axis=1)
+        z_padded = np.pad(self.spe_data, ((0, 1), (0, 0)), 'constant', constant_values=1)
+        self.spe_data = np.dot(self.tr_mat, z_padded)[:-1]
+    ###############################################
+    def get_point_chord_list(self):
+        if(not self.loaded):
+            return
+        point_chord_list = list()
+
+        min_x = self.dilate_data[0][0]
+        max_x = self.dilate_data[0][self.leading_edge_idx]
+        for i in self.dilate_data[0][:self.leading_edge_idx]:
+            point_chord_list.append((i - min_x) / (max_x - min_x) / 2)
+
+        point_chord_list.append(0.5)
+
+        min_x = self.dilate_data[0][-1]
+        for i in self.dilate_data[0][self.leading_edge_idx+1:]:
+            point_chord_list.append((1-((i - min_x) / (max_x - min_x))) / 2 + 0.5)
+
+        return point_chord_list
+    ###############################################
+    def __compute_leading_edge(self):
+        edge_candidates = np.where(self.orig_data[0] == 0)[0]
+        self.leading_edge_idx = None
         if(len(edge_candidates) == 0):
             print("Error : No candidate for leading edge, normalization could have failed.")
         elif(len(edge_candidates) == 1):
-            self.l_edge_idx = edge_candidates[0]
+            self.leading_edge_idx = edge_candidates[0]
         elif(len(edge_candidates) == 2):
             if(edge_candidates[0] + 1 != edge_candidates[1]):
                 print("Error : There exist two leading edge that are not contiguous.")
             else:
-                ys = np.take(self.file_data[1], edge_candidates)
+                ys = np.take(self.orig_data[1], edge_candidates)
                 middle_y = (ys[0] + ys[1]) / 2
-                self.file_data = np.insert(self.file_data, edge_candidates[1], [0, middle_y], axis=1)
-                self.l_edge_idx = edge_candidates[1]
+                self.orig_data = np.insert(self.orig_data, edge_candidates[1], [0, middle_y], axis=1)
+                self.leading_edge_idx = edge_candidates[1]
         else:
             print("Error : More than two leading edge candidates.")
 
-        return self.l_edge_idx is not None
+        return self.leading_edge_idx is not None
     ###############################################
     def get_point(self, val):
         if val <= 0.0 :
-            return self.points[0]
+            return np.take(self.dilate_data, 0, axis=1)
 
         if val == 0.5 :
-            return self.points[self.compute_leading_edge()]
+            return np.take(self.dilate_data, self.leading_edge_idx, axis=1)
 
         if val >= 1.0 :
-            return self.points[len(self.points)-1]
+            return np.take(self.dilate_data, -1, axis=1)
 
         if val < 0.5 :
-            x = 2 * (0.5 - val) * self.length()
-            x += self.points[self.compute_leading_edge()][0]
-            i = 0
-            while self.points[i][0] > x:
-                i += 1
+            min_x = self.dilate_data[0][0]
+            max_x = self.dilate_data[0][self.leading_edge_idx]
+            val_scaled = (max_x - min_x)*val*2 + min_x
 
-            next_p = self.points[i]
-            prev_p = self.points[i-1]
-            next_dist = x - next_p[0]
-            prev_dist = prev_p[0] - x
+            next_idx = np.argmax(self.dilate_data[0] >= val_scaled)
+            prev_idx = next_idx - 1
 
-            return (next_p * prev_dist + prev_p * next_dist) / (prev_dist + next_dist)
+            prev_p = np.take(self.dilate_data, prev_idx, axis=1)
+            next_p = np.take(self.dilate_data, next_idx, axis=1)
+
+            side_gap = next_p[0] - prev_p[0]
+            prev_gap = val_scaled - prev_p[0]
+            return prev_p + ((next_p - prev_p) * prev_gap / side_gap)
         else:
-            x = 2 * (val - 0.5) * self.length()
-            x += self.points[self.compute_leading_edge()][0]
-            i = self.compute_leading_edge()
-            while self.points[i][0] < x:
-                i += 1
+            min_x = self.dilate_data[0][-1]
+            max_x = self.dilate_data[0][self.leading_edge_idx]
+            val_scaled = (max_x - min_x)*(1-val)*2 + min_x
 
-            next_p = self.points[i]
-            prev_p = self.points[i-1]
-            next_dist = next_p[0] - x
-            prev_dist = x - prev_p[0]
+            next_idx = np.argmax(self.dilate_data[0][self.leading_edge_idx:] <= val_scaled) + self.leading_edge_idx
+            prev_idx = next_idx - 1
 
-            return (next_p * prev_dist + prev_p * next_dist) / (prev_dist + next_dist)
-    ###############################################
-    def get_normalized_pos(self, idx):
-        lead = self.compute_leading_edge()
-        length = self.length()
-        if idx <= 0 :
-            return 0.0
-        if idx == lead :
-            return 0.5
-        if idx >= len(self.points) - 1 :
-            return 1.0
+            prev_p = np.take(self.dilate_data, prev_idx, axis=1)
+            next_p = np.take(self.dilate_data, next_idx, axis=1)
 
-        if idx < lead :
-            return (length - self.points[idx][0] + self.points[lead][0]) / (2.0 * length)
-        else:
-            return (self.points[idx][0] - self.points[lead][0]) / (2.0 * length) + 0.5
-    ###############################################
-    def merge(self, airfoil):
-        counter_a = 1
-        counter_b = 1
-        output = list()
-        for i in range(4):
-            output.append(list())
-
-        output[0].append(self.points[0][0])
-        output[1].append(self.points[0][1])
-        output[2].append(airfoil.points[0][0])
-        output[3].append(airfoil.points[0][1])
-
-        while(counter_a != len(self.points-1) and counter_b != len(airfoil.points-1)):
-            npos_a = self.get_normalized_pos(counter_a)
-            npos_b = airfoil.get_normalized_pos(counter_b)
-
-            if npos_a == npos_b :
-                counter_a += 1
-                counter_b += 1
-                npos = npos_a
-            if npos_a > npos_b :
-                counter_a += 1
-                npos = npos_a
-            else:
-                counter_b += 1
-                npos = npos_b
-
-            p_a = self.get_point(npos)
-            p_b = airfoil.get_point(npos)
-            # output.append([p_a, p_b])
-            output[0].append(p_a[0])
-            output[1].append(p_a[1])
-            output[2].append(p_b[0])
-            output[3].append(p_b[1])
-
-        return np.array(output, float)
+            side_gap = prev_p[0] - next_p[0]
+            prev_gap = prev_p[0] - val_scaled
+            return prev_p + ((next_p - prev_p) * prev_gap / side_gap)
 ################################################################################
--- pywing.py
+++ pywing.py
@@ -3,6 +3,7 @@
 from PyQt5 import QtCore, QtGui, Qt
 import pyqtgraph as pg
 import sys, os
+import numpy as np
 
 from airfoil import Airfoil
 
@@ -11,10 +12,30 @@ airfoil_data_folder = QtCore.QDir.homePath() + "/.airfoils"
 airfoil_left = Airfoil()
 airfoil_right = Airfoil()
 
+fusion = pg.QtGui.QGraphicsPathItem()
+
+def airfoil_refresh_callback():
+    if(airfoil_left.loaded and airfoil_right.loaded):
+        l = airfoil_left.get_point_chord_list() + airfoil_right.get_point_chord_list()
+        l = list(set(l))
+        l.sort()
+
+        airfoil_right.get_points(l)
+        airfoil_left.get_points(l)
+
+        x = np.dstack((airfoil_right.spe_data[0], airfoil_left.spe_data[0]))
+        y = np.dstack((airfoil_right.spe_data[1], airfoil_left.spe_data[1]))
+        connect = np.dstack((np.ones(len(l)), np.zeros(len(l))))
+
+        path = pg.arrayToQPath(x.flatten(), y.flatten(), connect.flatten())
+        fusion.setPath(path)
+
+
 class AirfoilItemManager:
     def __init__(self, airfoil, color):
         self.airfoil = airfoil
         self.curve = pg.PlotCurveItem([], [], pen=pg.mkPen(color=color, width=2))
+        self.dot_curve = pg.ScatterPlotItem([], [], pen=pg.mkPen(color=color, width=2))
 
         self.load_btn = QtGui.QPushButton("Load")
         self.load_btn.clicked.connect(self.on_load)
@@ -62,7 +83,9 @@ class AirfoilItemManager:
         self.name.setStyleSheet("color: rgb" + color_str)
 
     def __refresh_curve(self):
-        self.curve.setData(self.airfoil.mod_data[0], self.airfoil.mod_data[1])
+        self.curve.setData(self.airfoil.trans_data[0], self.airfoil.trans_data[1])
+        self.dot_curve.setData(self.airfoil.spe_data[0], self.airfoil.spe_data[1])
+        airfoil_refresh_callback()
 
     def on_load(self):
         filename, _ = QtGui.QFileDialog.getOpenFileName(self.load_btn.parent(), "Open File", airfoil_data_folder, "All Files (*)")
@@ -99,8 +122,13 @@ class MainWidget(QtGui.QWidget):
         self.airfoil_right_view = AirfoilItemManager(airfoil_right,(233, 79, 55))
 
         plot = pg.PlotWidget()
+        fusion.setPen(pg.mkPen(0.7))
+        plot.addItem(fusion)
         plot.addItem(self.airfoil_left_view.curve)
+        plot.addItem(self.airfoil_left_view.dot_curve)
         plot.addItem(self.airfoil_right_view.curve)
+        plot.addItem(self.airfoil_right_view.dot_curve)
+
 
         grid_alpha = 50
         grid_levels = [(10, 0), (5, 0), (1, 0)]
